14c6e69b9157fa02248e1be1d481ba74467a4cb7
Manaus Transez <manaustransez@hotmail.com>
2021-04-02 20:08:08 +0200

    Removed static fonts

--- _nuxt/static/1617385447/7vX5yphSQ7Drut4moiuIXv/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1617385447",layout:"default",error:a,serverRendered:true,routePath:"\u002F7vX5yphSQ7Drut4moiuIXv",config:{app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
