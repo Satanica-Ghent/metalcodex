526f63cb1e8b5bbf0f2b0d641cfa50f098a90231
manaust <manaustransez@hotmail.com>
2021-04-04 13:49:55 +0200

    Changed apple statusbarstyle

---
+++ _nuxt/static/1617536806/0K0RKbYVnBNeLPW8rTM0Sq/payload.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("/0K0RKbYVnBNeLPW8rTM0Sq", (function(a){return {data:[{}],fetch:{"data-v-537c2218:0":{_error:{message:"request to https:\u002F\u002Fsatanica.be\u002Fapi\u002Fsongs.json failed, reason: connect ETIMEDOUT 185.199.110.153:443",type:"system",errno:a,code:a,statusCode:500}}},mutations:void 0}}("ETIMEDOUT")));
---
+++ _nuxt/static/1617536806/2H4PGNTvlEXDGcQV9Xe3m1/payload.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("/2H4PGNTvlEXDGcQV9Xe3m1", (function(a){return {data:[{}],fetch:{"data-v-537c2218:0":{_error:{message:"request to https:\u002F\u002Fsatanica.be\u002Fapi\u002Fsongs.json failed, reason: connect ETIMEDOUT 185.199.110.153:443",type:"system",errno:a,code:a,statusCode:500}}},mutations:void 0}}("ETIMEDOUT")));
